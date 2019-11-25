3e00f05a13ed0ff6065c7b605f1d98ee1307b3c8
Алексей Иванов <Alessio18911@gmail.com>
2019-11-25 18:29:04 +0300

    First Commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,73 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const posthtml = require('gulp-posthtml');
+const include = require('posthtml-include');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const csso = require('gulp-csso');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const build = series(clean, copy, parallel(css, js), html);
+
+function html() {
+  return src('source/*.html')
+    .pipe(posthtml([include()]))
+    .pipe(dest('dist'));
+}
+
+function css() {
+  return src('source/scss/style.scss', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csso())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('dist/css', { sourcemaps: true }))
+    .pipe(browserSync.stream());
+}
+
+function js() {
+  return src('source/js/*', { sourcemaps: true })
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('dist/js', { sourcemaps: true }));
+}
+
+function copy() {
+  return src([
+    'source/images/*',
+    'source/fonts/*'], { base: 'source'})
+    .pipe(dest('./dist'));
+}
+
+function clean() {
+  return del('dist');
+}
+
+function server() {
+  browserSync.init({
+    server: {
+        baseDir: './dist'
+    }
+  });
+
+  watch("source/*.html", series(html, refreshPage));
+  watch("source/scss/**/*", css);
+  watch("source/js/**/*", series(js, refreshPage));
+}
+
+function refreshPage(done) {
+  browserSync.reload();
+  done();
+}
+
+exports.default = series(build, server);
+exports.build = build;
+exports.clean = clean;
---
+++ source/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let a = "Hello, World";
+
+const fn = (b) => {
+  console.log(b);
+};
+
+fn(a);

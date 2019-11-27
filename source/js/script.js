7b42b68b93e89e31f8c886242c20f1457b076d26
Алексей Иванов <Alessio18911@gmail.com>
2019-11-27 17:20:38 +0300

    Мобильное разрешение перевёрстка

--- source/js/script.js
+++ source/js/script.js
@@ -4,7 +4,7 @@ const logoSvg = document.querySelector('.logo__svg');
 const logoLetter = document.querySelector('.logo__letter');
 const themeToggle = document.querySelector('#theme-toggle');
 const toggleMenuBtn = document.querySelector('.burger');
-const mainMenu = document.querySelector('.page-header__nav-wrapper');
+const mainMenu = document.querySelector('.page-header__wrapper--right');
 const mainMenuItems = document.querySelectorAll('.main-nav__link');
 
 function onThemeToggleChange() {
@@ -13,7 +13,7 @@ function onThemeToggleChange() {
 
 function onToggleMenuBtnClick() {
   toggleMenuBtn.classList.toggle('burger--active');
-  mainMenu.classList.toggle('page-header__nav-wrapper--opened');
+  mainMenu.classList.toggle('page-header__wrapper--opened');
   logoSvg.classList.toggle('logo__svg--white');
   logoLetter.classList.toggle('logo__letter--black');
   document.body.classList.toggle('body--immobile');

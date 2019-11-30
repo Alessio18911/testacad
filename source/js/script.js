4ef71e998ccf1a4514b6524c47bfd9c027fa43bc
Алексей Иванов <Alessio18911@gmail.com>
2019-11-30 21:08:14 +0300

    Рефакторинг с switch

--- source/js/script.js
+++ source/js/script.js
@@ -37,33 +37,50 @@ const toggleMenuBtn = body.querySelector(".burger");
 const mainMenu = body.querySelector(".page-header__wrapper--right");
 const mainMenuItems = body.querySelectorAll(".main-nav__link");
 
-function toggleMainMenuItemsColor(evt) {
-  mainMenuItems.forEach(item => changeProps(item, BlocksMap.mainMenuItem.prop));
-  changeProps(evt.target, BlocksMap.mainMenuItem.prop, ActionCode.ADD_CLASS);
+function changeProps(elementsAndProps) {
+  elementsAndProps.forEach(item => {
+    let element = item[0];
+    let elementClass = item[1];
+    let code = item[2];
+
+    switch (code) {
+      case 2:
+        element.classList.toggle(elementClass);
+        break;
+
+      case 1:
+        element.classList.add(elementClass);
+        break;
+
+      default:
+        element.classList.remove(elementClass);
+    }
+  });
 }
 
-function changeProps(blockName, propClass, code) {
-  switch(code) {
-    case 2: blockName.classList.toggle(propClass);
-      break;
-
-    case 1: blockName.classList.add(propClass);
-      break;
-
-    default: blockName.classList.remove(propClass);
-  }
+function toggleMainMenuItemsColor(evt) {
+  mainMenuItems.forEach(item =>
+    changeProps([[item, BlocksMap.mainMenuItem.prop]])
+  );
+  changeProps([
+    [evt.target, BlocksMap.mainMenuItem.prop, ActionCode.ADD_CLASS]
+  ]);
 }
 
 function onThemeToggleChange() {
-  changeProps(body, BlocksMap.body.props.color, ActionCode.TOGGLE_CLASS);
-  changeProps(logo, BlocksMap.logo.prop, ActionCode.TOGGLE_CLASS);
+  changeProps([
+    [body, BlocksMap.body.props.color, ActionCode.TOGGLE_CLASS],
+    [logo, BlocksMap.logo.prop, ActionCode.TOGGLE_CLASS]
+  ]);
 }
 
 function onToggleMenuBtnClick() {
-  changeProps(toggleMenuBtn, BlocksMap.toggleMenuBtn.prop, ActionCode.TOGGLE_CLASS);
-  changeProps(mainMenu, BlocksMap.mainMenu.prop, ActionCode.TOGGLE_CLASS);
-  changeProps(logo, BlocksMap.logo.prop, ActionCode.TOGGLE_CLASS);
-  changeProps(body, BlocksMap.body.props.immobility, ActionCode.TOGGLE_CLASS);
+  changeProps([
+    [toggleMenuBtn, BlocksMap.toggleMenuBtn.prop, ActionCode.TOGGLE_CLASS],
+    [mainMenu, BlocksMap.mainMenu.prop, ActionCode.TOGGLE_CLASS],
+    [logo, BlocksMap.logo.prop, ActionCode.TOGGLE_CLASS],
+    [body, BlocksMap.body.props.immobility, ActionCode.TOGGLE_CLASS]
+  ]);
 }
 
 function onMainMenuItemClick(evt) {
@@ -71,25 +88,36 @@ function onMainMenuItemClick(evt) {
   toggleMainMenuItemsColor(evt);
 }
 
-mainMenuItems.forEach(item => item.addEventListener("click", onMainMenuItemClick));
+mainMenuItems.forEach(item =>
+  item.addEventListener("click", onMainMenuItemClick)
+);
 themeToggle.addEventListener("change", onThemeToggleChange);
 toggleMenuBtn.addEventListener("click", onToggleMenuBtnClick);
 
 window.addEventListener("resize", function(evt) {
-  if (window.innerWidth < 768) {
-    changeProps(body, BlocksMap.body.props.immobility);
-    changeProps(body, BlocksMap.body.props.color);
-    themeToggle.checked = false;
+  let windowWidth = window.innerWidth;
 
-    if(!toggleMenuBtn.classList.contains("burger--active")) {
-      changeProps(logo, BlocksMap.logo.prop);
-    }
-  } else {
-    changeProps(toggleMenuBtn, BlocksMap.toggleMenuBtn.prop);
-    changeProps(mainMenu, BlocksMap.mainMenu.prop);
+  switch(windowWidth < 768) {
+    case true:
+      changeProps([
+        [body, BlocksMap.body.props.immobility],
+        [body, BlocksMap.body.props.color]
+      ]);
+      themeToggle.checked = false;
 
-    if (!body.classList.contains("body--dark")) {
-      changeProps(logo, BlocksMap.logo.prop);
-    }
+      if (!toggleMenuBtn.classList.contains("burger--active")) {
+        changeProps([[logo, BlocksMap.logo.prop]]);
+      }
+      break;
+
+    default:
+      changeProps([
+        [toggleMenuBtn, BlocksMap.toggleMenuBtn.prop],
+        [mainMenu, BlocksMap.mainMenu.prop]
+      ]);
+
+      if (!body.classList.contains("body--dark")) {
+        changeProps([[logo, BlocksMap.logo.prop]]);
+      }
   }
 });
